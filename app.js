ce6d99299e510c09186c718bae3a77d7b8315891
kevinsuu <mb0g0107@stust.edu.tw>
2024-03-20 21:01:22 +0800

    加入 SwaggerAPI

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./data/swagger.json"); // 您的 Swagger JSON 文件的路徑
 
 // 設定 body-parser 以處理 POST 請求
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // 設定路由
 const routes = require("./routes");
